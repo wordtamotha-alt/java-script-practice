0922fe2ed6bdcc48271e6a3f630614f476479fba
wordtamotha-alt <wordtamotha@gmail.com>
2026-08-23 21:21:51 -0400

    Add variables practice exercise

---
+++ 01-basics/variables.js
@@ -0,0 +1,26 @@
+// ===== VARIABLES PRACTICE =====
+// Variables store values that you can use and change
+
+// 1. Using 'let' - can be changed later
+let name = "John";
+let age = 25;
+
+console.log("My name is: " + name);
+console.log("My age is: " + age);
+
+// Change the value
+age = 26;
+console.log("Next year I will be: " + age);
+
+// 2. Using 'const' - cannot be changed (constant)
+const city = "New York";
+console.log("I live in: " + city);
+
+// 3. Different data types
+let isStudent = true;  // boolean (true/false)
+let salary = 50000.50; // number (decimal)
+let hobby = "coding";  // string (text)
+
+console.log("Is student: " + isStudent);
+console.log("Salary: $" + salary);
+console.log("Hobby: " + hobby);
